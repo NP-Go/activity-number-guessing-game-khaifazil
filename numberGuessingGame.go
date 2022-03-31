d7972880b90690b05c7aec77118f324b9d6e9802
khaifazil <khairal.fazil@gmail.com>
2022-04-01 01:00:56 +0800

    completed activity

--- numberGuessingGame.go
+++ numberGuessingGame.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -9,6 +10,32 @@ func main() {
 	//This code creates random number and place as variable
 	rand.Seed(time.Now().UnixNano())
 	hiddenNumber := int64(rand.Intn(100))
-
 	//Insert code here
+	var userNum int64
+	fmt.Println("Guess the number between 1 to 100!")
+
+	for i := 5; i >= 0; i-- {
+		if i > 0 {
+			fmt.Printf("You have %v tries!\n", i)
+			fmt.Println("Enter 101 to exit program.")
+			fmt.Println("Enter number: ")
+			fmt.Scanln(&userNum)
+
+			if userNum == hiddenNumber {
+				fmt.Println("You guessed correct!")
+				break
+			} else if userNum != hiddenNumber {
+				fmt.Println("You guessed wrong!")
+			} else if userNum == 101 {
+				fmt.Println("Goodbye!")
+				break
+			} else {
+				fmt.Print("Error. That is not a number!")
+			}
+		} else {
+			fmt.Println("You ran out of tries! You Lose!")
+		}
+	}
+	fmt.Println("Exiting program.")
+
 }
